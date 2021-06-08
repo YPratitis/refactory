f38327ea6645709188ad8a586b79583bdf9b9708
yohanes123 <yohanespratitis@gmail.com>
2021-06-08 08:33:28 +0700

    Answer FitBuzz

--- Logic Test/FizzBuzz/index.js
+++ Logic Test/FizzBuzz/index.js
@@ -1,3 +1,24 @@
-const x = 10;
-const y = 15;
-console.log(x+y);
+var fizzBuzz = function(n) {
+    let results = [];
+    for(let i=1; i <= n; i++) {
+      let fizz = i % 3 === 0;
+      let buzz = i % 5 === 0;
+      if (!fizz && !buzz) {
+      	results.push(`${i}`);
+        continue;
+      }
+      if (fizz && buzz) {
+      	results.push("FizzBuzz");
+        continue;
+      }
+      if (fizz) {
+      	results.push("Fizz");
+        continue;
+      }
+      if (buzz) {
+      	results.push("Buzz");
+        continue;
+      }
+    }
+    return results;
+};
